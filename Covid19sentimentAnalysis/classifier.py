0ac53e4fad831805f7365d44301a5c79d9e679c8
PETROPOULOS Panos <ppetropo@intrasoft-intl.com>
2022-01-11 17:29:17 +0200

    update code

--- Covid19sentimentAnalysis/classifier.py
+++ Covid19sentimentAnalysis/classifier.py
@@ -1,5 +1,4 @@
 import itertools
-
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -15,18 +14,25 @@ import matplotlib.pyplot as plt
 from nltk.corpus import stopwords, wordnet
 import re, string
 from nltk import FreqDist, PorterStemmer, SnowballStemmer, Counter
-
+#from wordcloud import WordCloud,STOPWORDS
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.neighbors import KNeighborsClassifier
 
 nltk.download('wordnet')
-
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# import these modules
 from nltk.stem import WordNetLemmatizer
 # from nltk.stem.snowball import SnowballStemmer
 import joblib
 import time
 from textblob import TextBlob
-data = pd.read_csv(r"Corona_NLP_train.csv", encoding='ansi')
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
+from sklearn import feature_selection
+data = pd.read_csv(r"Datasets/Corona_NLP_train.csv", encoding='ansi')
+
 
 
 data["Sentiment"] = data["Sentiment"].replace('Extremely Negative', 'Negative', regex=True)
@@ -41,8 +47,8 @@ data["Sentiment"]=data["Sentiment"].replace('Negative', -1, regex=True)
 data["Sentiment"]=data["Sentiment"].replace('Positive', 1, regex=True)
 data["Sentiment"]=data["Sentiment"].replace('Neutral', 0, regex=True)
 
-
-
+# data = data[data["Sentiment"] != 0]
+# data.reset_index(drop=True, inplace=True)
 #short words/apostrophe lookup
 contraction_dict1 = {"Â":"","’":"'","Ã":""}
 contraction_dict2 = {"Â":"","’":"'","ain't": "are not","'s":" is","aren't": "are not","don't": "do not","Don't":"Do not",
@@ -97,7 +103,7 @@ contraction_dict2 = {"Â":"","’":"'","ain't": "are not","'s":" is","aren't": "
                      "y'all": 'you all', "y'all'd": 'you all would',
                      "y'all'd've": 'you all would have', "y'all're": 'you all are',
                      "y'all've": 'you all have', "you'd": 'you had / you would',
-                     "you'd've": 'you would have',"&amp":"and"
+                     "you'd've": 'you would have',"&amp":"and","btc":"bitcoin","irs":"","spx":"","📍":"","✅":""
                      }
 
 emoticons={':)': 'happy', ':‑)': 'happy',
@@ -112,7 +118,8 @@ emoticons={':)': 'happy', ':‑)': 'happy',
            '>:[': 'sad', ':{': 'sad',
            '>:(': 'sad', ':-c': 'sad',
            ':-< ': 'sad', ':-[': 'sad',
-           ':-||': 'sad'}
+           ':-||': 'sad',
+           '😢':'sad'         }
 
 myOwnStopWords={'price':"",
                 'store':"",
@@ -121,14 +128,17 @@ myOwnStopWords={'price':"",
                 'grocery':"",
                 'people':"",
                 'go':"",
-                'consumer':""}
+                'consumer':"",
+                'usdjpy':"", 'gbpusd':"", 'usdcnh':"", 'xauusd':"", 'wti':"", 'spx':"",'iave':"","aiave":""}
+
 
 def lookup_dict(text, dictionary):
-    for word in text.split():
-        if word.lower() in dictionary:
-            if word.lower() in text.split():
-                text = text.replace(word, dictionary[word.lower()])
-    return text
+    if isinstance(text, float) == False and text is not None:
+        for word in text.split():
+            if word.lower() in dictionary:
+                if word.lower() in text.split():
+                    text = text.replace(word, dictionary[word.lower()])
+        return text
 data["OriginalTweet"] = data["OriginalTweet"].apply(lambda x: lookup_dict(x,emoticons))
 
 data['OriginalTweet']=data['OriginalTweet'].apply(lambda x:lookup_dict(x,contraction_dict1))
@@ -164,13 +174,17 @@ data['OriginalTweet']  = data['OriginalTweet'].str.strip()
 data["OriginalTweet"]=data["OriginalTweet"].replace("_", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("   ", " ", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("  ", " ", regex=True)
+
 data['OriginalTweet']  = data['OriginalTweet'].str.strip()
 data["OriginalTweet"]=data["OriginalTweet"].replace("coronavir¼", "coronavirus", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("pmmodi", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("amp", "and", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace("btc", "bitcoin", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace("hand", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].fillna(0)
 
 data['OriginalTweet']  = data['OriginalTweet'].str.strip()
+
 
 #Tokenize the tweets
 tokenized_tweets = data["OriginalTweet"].apply(lambda x: x.split())
@@ -218,96 +232,153 @@ for line in list(data['OriginalTweet']):
 
 print(Counter(all_words).most_common(10))
 data["OriginalTweet"]=data["OriginalTweet"].replace("  ", " ", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace(" +", " ", regex=True)
+#
+# class_3,class_2,class_1 = data["Sentiment"].value_counts()
+#
+# c3 = data[data["Sentiment"] == -1]
+# c2 = data[data["Sentiment"] == 1]
+# c1 = data[data["Sentiment"] == 0]
+# df_3 = c3.sample(class_1)
+# df_2 = c2.sample(class_1)
+# undersampled_df = pd.concat([df_3,df_2,c1],axis=0)
+data['OriginalTweet']  = data['OriginalTweet'].str.strip()
+
+
 Y = data["Sentiment"]
 
-tfidf_vectorizer = TfidfVectorizer(stop_words='english',max_features=15000,ngram_range=(1,3))#min_df=0.0001
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english',max_features=20000)
 tfidf = tfidf_vectorizer.fit_transform(data["OriginalTweet"])
+# joblib.dump(tfidf_vectorizer,'tfidf_vectorizer.sav')
+
 print(tfidf.toarray().shape)
 
 
-import seaborn as sns
-from sklearn import feature_selection
 
 # sns.heatmap(tfidf.todense()[:,np.random.randint(0,tfidf.shape[1],100)]==0,vmin=0,vmax=1,cbar=False).set_title('Sparse Matrix')
 # plt.show()
 X_names = tfidf_vectorizer.get_feature_names_out()
-p_value_limit = 0.95
-
+p_value_limit = 0.97
+#this feature selection is ranking features with respect to their usefulness and is not used to make statements about statistical dependence or independence of variables.
 features = pd.DataFrame()
 for cat in np.unique(data["Sentiment"]):
-    chi2, p = feature_selection.chi2(tfidf,data["Sentiment"]==cat)
+    chi2, p = feature_selection.chi2(tfidf,data["Sentiment"]==cat)#chi2(tfidf,data["Sentiment"]==cat)
     features = features.append(pd.DataFrame({"feature":X_names,"score":1-p,"Y":cat}))
     features = features.sort_values(["Y","score"],ascending=[True,False])
+
     features = features[features['score']>p_value_limit]
+
+X_scores = features["score"].unique().tolist()
 X_names = features["feature"].unique().tolist()
 print(X_names)
+
 
 for cat in np.unique(data["Sentiment"]):
     print("# {}:".format(cat))
     print(" . selected features:", len(features[features["Y"]==cat]))
-    print(" . top features:",",".join(features[features["Y"]==cat]["feature"].values[:15]))
+    print(" . top features:",",".join(features[features["Y"]==cat]["feature"].values[:20]))
+    # print(" . top features scores:",",".join(str(features[features["Y"]==cat]["score"].values[:10])))
     print(" ")
 # defining parameter range
-tfidf_vectorizer = feature_extraction.text.TfidfVectorizer(vocabulary=X_names)
+
+tfidf_vectorizer = feature_extraction.text.TfidfVectorizer(vocabulary=X_names,ngram_range=(1,2))
 tfidf_new = tfidf_vectorizer.fit_transform(data["OriginalTweet"])
-# print(tfidf_new.toarray().shape)
+# joblib.dump(tfidf_vectorizer,'vectorizer.sav')
+print(tfidf_new.toarray().shape)
 dic_vocab = tfidf_vectorizer.vocabulary_
-# print(dic_vocab)
 
-# sns.heatmap(tfidf_new.todense()[:,np.random.randint(0,tfidf_new.shape[1],100)]==0,vmin=0,vmax=1,cbar=False).set_title('Sparse Matrix')
-# plt.show()
-
-X_train2, X_test2, y_train2, y_test2 = model_selection.train_test_split(tfidf_new, Y, test_size=0.25,shuffle=True,random_state=42)
+X_train2, X_test2, y_train2, y_test2 = model_selection.train_test_split(tfidf_new, Y, test_size=0.25,shuffle=True,random_state=0)
 
 
-
-clf1 = svm.SVC(kernel='rbf',C=1.3,probability=True).fit(X_train2,y_train2)
-y_pred = clf1.predict(X_test2)
-# predicted_prob = clf1.predict_proba(X_test2)
-m_confusion_test = metrics.confusion_matrix(y_test2, clf1.predict(X_test2))
-print("SVM RBF")
-print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Predicted -1', 'Predicted 0','Predicted 1']))
-print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
-print(metrics.classification_report(y_pred,y_test2))
-
-clf2 = naive_bayes.ComplementNB(alpha=0.40).fit(X_train2,y_train2)
-y_pred = clf2.predict(X_test2)
-predicted_prob = clf2.predict_proba(X_test2)
-m_confusion_test = metrics.confusion_matrix(y_test2, clf2.predict(X_test2))
-print("NB")
-print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Actual -1','Actual 0','Actual 1']))
-print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
-print(metrics.classification_report(y_pred,y_test2))
-
-clf3 = svm.SVC(kernel='linear',probability=True).fit(X_train2,y_train2)
-y_pred = clf3.predict(X_test2)
-# predicted_prob = clf1.predict_proba(X_test2)
-m_confusion_test = metrics.confusion_matrix(y_test2, clf3.predict(X_test2))
-print("SVM Linear")
-print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Predicted -1', 'Predicted 0','Predicted 1']))
-print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
-print(metrics.classification_report(y_pred,y_test2))
+list_alpha = np.arange(1/100000, 10, 0.1)
+param_grid = {'alpha':list_alpha
 
 
-clf4 = KNeighborsClassifier().fit(X_train2,y_train2)
-y_pred = clf4.predict(X_test2)
-# predicted_prob = clf1.predict_proba(X_test2)
-m_confusion_test = metrics.confusion_matrix(y_test2, clf4.predict(X_test2))
-print("KNN")
-print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Predicted -1', 'Predicted 0','Predicted 1']))
-print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
-print(metrics.classification_report(y_pred,y_test2))
+               }
+grid = GridSearchCV(naive_bayes.ComplementNB(), param_grid, refit = True, verbose = 5)
+#ComplementNB()
+
+grid.fit(X_train2, y_train2)
+# print best parameter after tuning
+print(grid.best_params_)
+grid_predictions = grid.predict(X_test2)
+#
+# # print classification report
+print(metrics.classification_report(y_test2, grid_predictions))
+
+param_grid = {'C':[0.01,0.1,1,2,10,100,1000,2000,5000]
+
+
+              }
+grid = GridSearchCV(svm.SVC(kernel='rbf'), param_grid, refit = True, verbose = 5)
+
+
+grid.fit(X_train2, y_train2)
+# print best parameter after tuning
+print(grid.best_params_)
+grid_predictions = grid.predict(X_test2)
+
+# print classification report
+print(metrics.classification_report(y_test2, grid_predictions))
+
+param_grid = {'C':[0.001, 0.01, 0.1, 1.0,1.1,1.2,10,100,1000],
+              'max_iter': [200,500,1000],
+              'solver':['newton-cg','lbfgs']
+              }
+grid = GridSearchCV(LogisticRegression(multi_class='multinomial'), param_grid, refit = True, verbose = 5)
+
+
+grid.fit(X_train2, y_train2)
+# print best parameter after tuning
+print(grid.best_params_)
+grid_predictions = grid.predict(X_test2)
+
+# print classification report
+print(metrics.classification_report(y_test2, grid_predictions))
 
 
 
-data = pd.read_csv(r"Corona_NLP_test.csv", encoding='ansi')
+# clf1 = svm.SVC(kernel='rbf',C=1.3,probability=True).fit(X_train2,y_train2)
+# y_pred = clf1.predict(X_test2)
+# predicted_prob1 = clf1.predict_proba(X_test2)
+# m_confusion_test = metrics.confusion_matrix(y_test2, clf1.predict(X_test2))
+# print("SVM RBF")
+# print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Predicted -1', 'Predicted 0','Predicted 1']))
+# print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
+# print(metrics.classification_report(y_pred,y_test2))
+#
+# clf2 = naive_bayes.ComplementNB(alpha=0.40).fit(X_train2,y_train2)
+# y_pred = clf2.predict(X_test2)
+# predicted_prob2 = clf2.predict_proba(X_test2)
+# m_confusion_test = metrics.confusion_matrix(y_test2, clf2.predict(X_test2))
+# print("NB")
+# print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Actual -1','Actual 0','Actual 1']))
+# print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
+# print(metrics.classification_report(y_pred,y_test2))
+#
+# clf3 = LogisticRegression().fit(X_train2,y_train2)
+# y_pred = clf3.predict(X_test2)
+# predicted_prob3 = clf1.predict_proba(X_test2)
+# m_confusion_test = metrics.confusion_matrix(y_test2, clf3.predict(X_test2))
+# print("SVM Linear")
+# print(pd.DataFrame(data = m_confusion_test , columns=['Predicted -1', 'Predicted 0','Predicted 1'],index=['Predicted -1', 'Predicted 0','Predicted 1']))
+# print("Accuracy:",metrics.accuracy_score(y_test2, y_pred))
+# print(metrics.classification_report(y_pred,y_test2))
+
+
+
+
+
+data = pd.read_csv(r"Datasets/Corona_NLP_test.csv", encoding='ansi')
 data["Sentiment"] = data["Sentiment"].replace('Extremely Negative', 'Negative', regex=True)
 
 data["Sentiment"] = data["Sentiment"].replace('Extremely Positive', 'Positive', regex=True)
 data["Sentiment"]=data["Sentiment"].replace('Negative', -1, regex=True)
 data["Sentiment"]=data["Sentiment"].replace('Positive', 1, regex=True)
 data["Sentiment"]=data["Sentiment"].replace('Neutral', 0, regex=True)
-#to lower case
+
+
 data["OriginalTweet"] = data["OriginalTweet"].apply(lambda x: lookup_dict(x,emoticons))
 
 data['OriginalTweet']=data['OriginalTweet'].apply(lambda x:lookup_dict(x,contraction_dict1))
@@ -343,20 +414,18 @@ data['OriginalTweet']  = data['OriginalTweet'].str.strip()
 data["OriginalTweet"]=data["OriginalTweet"].replace("_", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("   ", " ", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("  ", " ", regex=True)
-# data["OriginalTweet"]=data["OriginalTweet"].replace("rn", "", regex=True)
-# data["OriginalTweet"]=data["OriginalTweet"].replace("vr", "", regex=True)
-# data["OriginalTweet"]=data["OriginalTweet"].replace("hl", "", regex=True)
-# data["OriginalTweet"]=data["OriginalTweet"].replace("u", "", regex=True)
-# data["OriginalTweet"]=data["OriginalTweet"].replace("ot", "", regex=True)
+
 data['OriginalTweet']  = data['OriginalTweet'].str.strip()
 data["OriginalTweet"]=data["OriginalTweet"].replace("coronavir¼", "coronavirus", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("pmmodi", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].replace("amp", "and", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace("btc", "bitcoin", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace("hand", "", regex=True)
 data["OriginalTweet"]=data["OriginalTweet"].fillna(0)
 
 data['OriginalTweet']  = data['OriginalTweet'].str.strip()
-# print(data["OriginalTweet"][16])
-# print(data["OriginalTweet"].head(25))
+
+
 #Tokenize the tweets
 tokenized_tweets = data["OriginalTweet"].apply(lambda x: x.split())
 
@@ -364,14 +433,17 @@ tokenized_tweets = data["OriginalTweet"].apply(lambda x: x.split())
 stop_words = set(stopwords.words('english'))
 tokenized_tweets = tokenized_tweets.apply(lambda x: [word for word in x if not word in stop_words])
 
+#Stemming the words
 # stemmer = PorterStemmer()#language='english'
 # tokenized_tweets= tokenized_tweets.apply(lambda x: [stemmer.stem(i) for i in x])
 
+#get the lemma
 lemmatizer = WordNetLemmatizer()
+
 tokenized_tweets = tokenized_tweets.apply(lambda x: [lemmatizer.lemmatize(i,get_pos( i )) for i in x])
 
 tokenized_tweets = tokenized_tweets.apply(lambda x: [word for word in x if len(word)>2 or word=='go'])
-# tokenized_tweets= tokenized_tweets.apply(lambda x: [stemmer.stem(i) for i in x])
+
 
 #Joining the tokenized tweets
 for i in range(len(tokenized_tweets)):
@@ -387,7 +459,19 @@ for line in list(data['OriginalTweet']):
         all_words.append(word.lower())
 
 
+print(Counter(all_words).most_common(10))
 data["OriginalTweet"]=data["OriginalTweet"].replace("  ", " ", regex=True)
+data["OriginalTweet"]=data["OriginalTweet"].replace(" +", " ", regex=True)
+#
+# class_3,class_2,class_1 = data["Sentiment"].value_counts()
+#
+# c3 = data[data["Sentiment"] == -1]
+# c2 = data[data["Sentiment"] == 1]
+# c1 = data[data["Sentiment"] == 0]
+# df_3 = c3.sample(class_1)
+# df_2 = c2.sample(class_1)
+# undersampled_df = pd.concat([df_3,df_2,c1],axis=0)
+data['OriginalTweet']  = data['OriginalTweet'].str.strip()
 # remove stopwords in order to have only the full-meaning words (remove and , to etc)
 # data["OriginalTweet"] = data["OriginalTweet"].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
 
@@ -401,11 +485,11 @@ tfidf= tfidf_vectorizer.transform(data["OriginalTweet"])
 # print("SVM")
 # print(duration)
 
-start2 = time.time()
-y_pred2= clf2.predict(tfidf)
-duration2 = time.time() - start2
-print("NB")
-print(duration2)
+# start2 = time.time()
+# y_pred2= clf2.predict(tfidf)
+# duration2 = time.time() - start2
+# print("NB")
+# print(duration2)
 
 # start3 = time.time()
 # y_pred3= sclf.predict(tfidf)
@@ -414,5 +498,5 @@ print(duration2)
 # print(duration3)
 
 # print("Accuracy:",metrics.accuracy_score(data["Sentiment"], y_pred))
-print("Accuracy:",metrics.accuracy_score(data["Sentiment"], y_pred2))
+# print("Accuracy:",metrics.accuracy_score(data["Sentiment"], y_pred2))
 # print("Accuracy:",metrics.accuracy_score(data["Sentiment"], y_pred3))
